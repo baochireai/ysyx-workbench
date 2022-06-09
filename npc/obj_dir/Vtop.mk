216e79cef2e19eaf35a5a2859132c6e725d9002c
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-09 20:02:21 +0800

    > sim RTL ysyx_22041329 薛贵 Linux mrxue 5.4.0-105-generic #119~18.04.1-Ubuntu SMP Tue Mar 8 11:21:24 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:02:21 up  2:18,  5 users,  load average: 0.07, 0.13, 0.09

--- npc/obj_dir/Vtop.mk
+++ npc/obj_dir/Vtop.mk
@@ -4,7 +4,7 @@
 # Execute this makefile from the object directory:
 #    make -f Vtop.mk
 
-default: Vtop__ALL.a
+default: Vtop
 
 ### Constants...
 # Perl executable (from $PERL)
@@ -53,5 +53,16 @@ VM_USER_DIR = \
 include Vtop_classes.mk
 # Include global rules
 include $(VERILATOR_ROOT)/include/verilated.mk
+
+### Executable rules... (from --exe)
+VPATH += $(VM_USER_DIR)
+
+sim_main.o: /home/mrxue/ysyx-workbench/npc/csrc/sim_main.cpp
+	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
+
+### Link rules... (from --exe)
+Vtop: $(VK_USER_OBJS) $(VK_GLOBAL_OBJS) $(VM_PREFIX)__ALL.a $(VM_HIER_LIBS)
+	$(LINK) $(LDFLAGS) $^ $(LOADLIBES) $(LDLIBS) $(LIBS) $(SC_LIBS) -o $@
+
 
 # Verilated -*- Makefile -*-
