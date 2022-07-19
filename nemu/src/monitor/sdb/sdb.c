b52547b97678de7ec19afa2137f3966a3796c420
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-19 14:35:50 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-120-generic #136~18.04.1-Ubuntu SMP Fri Jun 10 18:00:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:35:50 up  1:43,  1 user,  load average: 0.43, 0.33, 0.29

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -79,7 +79,7 @@ static int cmd_x(char *args){
     return 0;
   }
   for(int i=0;i<N;i++){
-    printf("%x\t",(unsigned int)paddr_read(Addr+i,8));
+    printf("%x ",(unsigned int)paddr_read(Addr+i,8));
     if((i+1)%4==0){
       printf("\n");
     }
