08b5958e4a6134843704578e4e90b1adb6154cae
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 20:10:08 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-122-generic #138~18.04.1-Ubuntu SMP Fri Jun 24 14:14:03 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:10:08 up  2:10,  1 user,  load average: 0.34, 0.35, 0.36

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -25,7 +25,7 @@ enum {
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
-static word_t immJ(uint32_t i) { return (SEXT(BITS(i,31,31),1)<<20) | BITS(i,30,21)<<1 | BITS(i,20,20)<<11 | BITS(i,30,23);}
+static word_t immJ(uint32_t i) { return (SEXT(BITS(i,31,31),1)<<20) | BITS(i,30,21)<<1 | BITS(i,20,20)<<11 | BITS(i,19,12);}
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
   //RSICV  所有指令 寄存器索引放在固定位置 
@@ -61,7 +61,7 @@ static int decode_exec(Decode *s) {
 
   // Myself
   INSTPAT("??????? ????? ????? 000 ????? 00100 11",addi,I,R(dest)=src1+src2);
-  INSTPAT("??????? ????? ????? ??? ????? 11011 11",jal,J,R(dest)=s->pc+4;s->pc=s->pc+2*src1);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11",jal,J,R(dest)=s->pc+4;s->pc=s->pc+src1);
 
 
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));  //前面所有指令均不匹配，视为非法指令
