5070dbb50a117f363e8ff5966c57ed59f9ff8fb5
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-19 16:33:38 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-124-generic #140~18.04.1-Ubuntu SMP Fri Aug 5 11:43:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:33:38 up  2:47,  1 user,  load average: 0.42, 0.37, 0.39

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,43 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+char* int2string(char* dst,int num){
+  char buff[20];
+  size_t len=0;
+  while(num){
+    int ch=num%10+'0';
+    buff[len]=(char)ch;
+    num=num/10;
+  }
+  for(size_t i=0;i<len;i++){
+    dst[i]=buff[len-i-1];
+  }
+  dst[len]='\0';
+  return dst;
+}
+
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  size_t len=0,i=0;
+  while(fmt[i]!='\0'){
+    if(fmt[i]=='%'&&fmt[i+1]=='s'){
+      char* str= va_arg(ap, char*); 
+      out[len]='\0';
+      strcat(out,str);
+      len=len+strlen(str);
+      i+=2;continue;
+    }
+    else if(fmt[i]=='%'&&fmt[i+1]=='d'){
+      int num=va_arg(ap,int);
+      int2string(out+len,num);
+      len=len+strlen(out+len);
+      i+=2;continue;
+    }
+    out[len++]=fmt[i];
+    i++;
+  }
+  return 1;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
