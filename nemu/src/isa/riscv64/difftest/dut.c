adc1d18e1ededc5ea1b05baacced8450189cc0aa
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-21 10:56:52 +0800

    > run NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-124-generic #140~18.04.1-Ubuntu SMP Fri Aug 5 11:43:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:56:52 up 1 day,  3:25,  1 user,  load average: 1.59, 1.51, 1.36

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -3,7 +3,6 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  //if(ref_r->pc!=pc) return false;
   for(int i=0;i<32;i++){
     if(ref_r->gpr[i]!=gpr(i)) return false;
   }
