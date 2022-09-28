34066c78a2532a78affeb5800bad9690d80e314e
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-28 21:27:22 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-125-generic #141~18.04.1-Ubuntu SMP Thu Aug 11 20:15:56 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:27:22 up 12:19,  1 user,  load average: 0.65, 0.75, 0.58

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -40,7 +40,7 @@ void init_map() {
 }
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
-  //printf("map read at 0x%8x\n",addr);
+  printf("map read at 0x%8x,%s\n",addr,map->name);
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;//地址偏移
