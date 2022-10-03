4939d59141d5067011f77d04e7a0be607c8d9af2
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-03 10:29:57 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-126-generic #142~18.04.1-Ubuntu SMP Thu Sep 1 16:25:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:29:57 up  1:56,  1 user,  load average: 0.43, 0.51, 0.45

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -23,8 +23,6 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  static int i=0;
-  i++;
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
@@ -34,17 +32,17 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     int block_w=ctl->w,block_h=ctl->h;
     int w = inw(VGACTL_ADDR+2);
     int h=inw(VGACTL_ADDR);
-    printf("%d\n",i%(32*32));
     for(int j=0;j<block_h;j++){
       for(int i=0;i<block_w;i++){
         int x=ctl->x+i,y=ctl->y+j;
         if(x<w&&y<h){
-          //printf("")
+          printf("(%d,%d)\n",x,y);
           // outl(FB_ADDR+y*w+x,((uint32_t*)ctl->pixels)[j*block_w+i]);
           outl(FB_ADDR+y*w+x,((uint32_t*)ctl->pixels)[j*block_w+i]);
         }
       }
     }
+    printf("###############################\n");
   }
 }
 
