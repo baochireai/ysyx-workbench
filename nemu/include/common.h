80c9d7e0024af3ebb407f2d6819fe0f99ee78092
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-05 10:26:57 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-126-generic #142~18.04.1-Ubuntu SMP Thu Sep 1 16:25:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:26:57 up  1:35,  1 user,  load average: 1.29, 0.78, 0.47

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -19,7 +19,7 @@
 #define PMEM64 1
 #endif
 
-volatile static int dummy;
+volatile static int dummy=0;
 
 typedef MUXDEF(CONFIG_ISA64, uint64_t, uint32_t) word_t;
 typedef MUXDEF(CONFIG_ISA64, int64_t, int32_t)  sword_t;
