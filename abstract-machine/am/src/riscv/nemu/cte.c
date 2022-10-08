4e14677e91efb2761938280b2e430c726c6bf5b8
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-08 21:17:54 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-126-generic #142~18.04.1-Ubuntu SMP Thu Sep 1 16:25:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:17:54 up  2:07,  1 user,  load average: 1.16, 0.66, 0.59

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -23,9 +23,12 @@ extern void __am_asm_trap(void);
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));//异常入口地址
-
+  /*
+  内联汇编，插入汇编代码（__am_asm_trap为汇编程序标识符）
+  __am_asm_trap中实现了异常处理过程（从保存pc，设置mcase的等，到进入用户异常处理程序，再mret返回恢复pc）
+  */
   // register event handler
-  user_handler = handler;//时间处理回调函数
+  user_handler = handler;//用户事件处理回调函数
 
   return true;
 }
@@ -35,7 +38,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 void yield() {
-  asm volatile("li a7, -1; ecall");
+  asm volatile("li a7, -1; ecall");//触发异常 进入mtvec的异常入口地址（进行异常处理流程）
 }
 
 bool ienabled() {
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,6 @@
 #include <common.h>
 
-static Context* do_event(Event e, Context* c) {
+static Context* do_event(Event e, Context* c) {//异常处理程序
   switch (e.event) {
     default: panic("Unhandled event ID = %d", e.event);
   }
@@ -10,5 +10,5 @@ static Context* do_event(Event e, Context* c) {
 
 void init_irq(void) {
   Log("Initializing interrupt/exception handler...");
-  cte_init(do_event);
+  cte_init(do_event);//传入异常处理程序入口地址
 }
