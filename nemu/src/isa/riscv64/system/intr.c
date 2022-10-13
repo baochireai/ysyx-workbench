f5975001f0fa35d0b551db5a4c328de93921bfce
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-13 11:21:20 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-128-generic #144~18.04.1-Ubuntu SMP Thu Sep 22 11:08:41 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:21:20 up  2:00,  1 user,  load average: 0.75, 0.75, 0.65

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -5,7 +5,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
   cpu.csr[1]=epc;//mepc
-  cpu.csr[2]=-1;
+  cpu.csr[2]=NO;
   return cpu.csr[5];//mtvec
 }
 
