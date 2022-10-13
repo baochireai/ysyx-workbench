d0b548ce259c416970ac49d0ca0840f36278fbc1
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-13 10:18:07 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-128-generic #144~18.04.1-Ubuntu SMP Thu Sep 22 11:08:41 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:18:07 up 56 min,  1 user,  load average: 0.65, 0.73, 0.67

--- nemu/src/isa/riscv64/include/isa-def.h
+++ nemu/src/isa/riscv64/include/isa-def.h
@@ -7,10 +7,11 @@ typedef struct {
   word_t gpr[32];
   vaddr_t pc;
   //控制状态寄存器CSR
-  word_t mstatus;//存放处理器状态 
-  word_t mcause;//存放触发异常的原因  进入对应异常处理程序
-  word_t mtvec;//存放异常入口地址 
-  vaddr_t mepc;//存放发生异常的pc 用于异常处理返回
+  word_t csr[8];
+  // csr[0] mstatus;//存放处理器状态 
+  // csr[1] mepc;//存放发生异常的pc 用于异常处理返回
+  // csr[2] mcause;//存放触发异常的原因  进入对应异常处理程序
+  // csr[5] mtvec;//存放异常入口地址 
 } riscv64_CPU_state;
 
 // decode
