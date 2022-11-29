f35b01e36a1695c3d949004e559f2a7f9fac7efd
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-29 19:41:17 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-132-generic #148~18.04.1-Ubuntu SMP Mon Oct 24 20:41:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:41:17 up  6:01,  1 user,  load average: 0.24, 0.19, 0.27

--- npc/csrc/cpu/cpu_exec.c
+++ npc/csrc/cpu/cpu_exec.c
@@ -41,8 +41,12 @@ void cpu_exec(uint64_t n){
 #ifdef CONFIG_DIFFTEST
     vaddr_t pc=cpu.pc;
     unsigned int Inst_RTL=top->Inst;
+
     //printf("Inst_RTL=%08x\n",Inst_RTL);
     cpu_exec_once();
+    if(cpu.pc==0x80001300){
+      difftest_skip_ref();
+    }
     //printf("pc=%lx\tdpc=%lx\n",pc,top->pc);
     difftest_step(pc,cpu.pc);
 #else
