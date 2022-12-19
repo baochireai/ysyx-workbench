4ad509d4bf28db36c8a7cd20478999c7170b9a2b
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-19 20:47:57 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-135-generic #152~18.04.2-Ubuntu SMP Tue Nov 29 08:23:49 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:47:57 up 4 min,  1 user,  load average: 0.72, 0.64, 0.31

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,7 @@
 #include <common.h>
 #include "syscall.h"
 
+#define strace
 void SYS_yield(){
   asm volatile("li a7, -1; ecall");
 }
@@ -10,6 +11,18 @@ void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;//syscall number
   //printf("GPR2:%d")
+#ifdef strace
+  switch (a[0]) {
+    case 0:
+      printf("halt(a[0])\n");
+      halt(a[0]);break;
+    case 1:
+      printf("SYS_yield()\t=");
+      SYS_yield();c->GPRx=0;
+      break;
+    default: panic("Unhandled syscall ID = %d", a[0]);
+  }
+#elif
   switch (a[0]) {
     case 0:
       halt(a[0]);break;
@@ -17,4 +30,5 @@ void do_syscall(Context *c) {
       SYS_yield();c->GPRx=0;break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+#endif
 }
