e4e55d6a48d910bb18e495d91d20e5012740a68d
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-20 18:18:54 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-135-generic #152~18.04.2-Ubuntu SMP Tue Nov 29 08:23:49 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:18:54 up  1:59,  3 users,  load average: 0.31, 0.54, 0.44

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -35,7 +35,7 @@ void do_syscall(Context *c) {
       break;
     }
     case SYS_brk://用户栈区内存申请
-      c->GPRx=0;
+      c->GPRx=0;//返回0 栈区调整成功
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
