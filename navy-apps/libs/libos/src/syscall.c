b50de92e32ef1d1f7993f86aefcd316dc6d846fa
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-20 16:47:08 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-135-generic #152~18.04.2-Ubuntu SMP Tue Nov 29 08:23:49 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:47:08 up 27 min,  3 users,  load average: 1.16, 0.60, 0.51

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -62,12 +62,13 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _syscall_(SYS_write, fd, buf, count);
-  return 0;
+  return _syscall_(SYS_write, fd, buf, count);
+  //return 0;
 }
 
 void *_sbrk(intptr_t increment) {
-  return (void *)-1;
+  return _syscall_(SYS_brk,increment);
+  //return (void *)-1;
 }
 
 int _read(int fd, void *buf, size_t count) {
