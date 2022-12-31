73f5430ca45f5bd087fce898a5846c8b5199a96b
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-31 19:57:29 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-135-generic #152~18.04.2-Ubuntu SMP Tue Nov 29 08:23:49 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:57:29 up 1 day,  8:29,  1 user,  load average: 0.95, 0.77, 0.60

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -5,10 +5,10 @@ int main() {
   printf("Timer Test\n");
   int half_sec = 1;
   struct timeval *tv;
-  gettimeofday(tv);
+  gettimeofday(tv,NULL);
   while(1){
     while (tv->tv_usec/500000<half_sec){
-      gettimeofday(tv);
+      gettimeofday(tv,NULL);
     }
     half_sec++;
     printf("%d half_secs pass \n",half_sec);
