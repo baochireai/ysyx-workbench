a4b78752c4be8132e8aab0cb686a93688a6dfffe
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-03 01:02:48 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-135-generic #152~18.04.2-Ubuntu SMP Tue Nov 29 08:23:49 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:02:48 up 16:35,  1 user,  load average: 0.78, 0.61, 0.49

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,8 +23,8 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  AM_INPUT_KEYBRD_T kbd;
-  ioe_read(AM_INPUT_KEYBRD,&kbd);
+  AM_INPUT_KEYBRD_T kbd = io_read(AM_INPUT_KEYBRD);
+  sprintf(buf,"%s %s\n", kbd.keydown ? "kd" : "ku",keyname[kbd.keycode]);
   return 0;
 }
 
