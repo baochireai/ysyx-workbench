40f119807be9d604479bcb6e4b47989fcf505ff0
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-06 14:50:02 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-135-generic #152~18.04.2-Ubuntu SMP Tue Nov 29 08:23:49 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:50:02 up  4:19,  1 user,  load average: 0.97, 0.70, 0.44

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -5,8 +5,7 @@ int main() {
   NDL_Init(0);
   while (1) {
     char buf[64];
-    size_t length=NDL_PollEvent(buf, sizeof(buf));
-    if (length!=0) {
+    if (NDL_PollEvent(buf, sizeof(buf))) {
       printf("receive event: %s\n", buf);
     }
   }
