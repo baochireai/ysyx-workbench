1b4b7df75eb704a3590d3aa942c0b87d3bd82723
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-08 21:28:31 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-136-generic #153~18.04.1-Ubuntu SMP Wed Nov 30 15:47:57 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:28:31 up  2:17,  1 user,  load average: 0.96, 0.24, 0.15

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -20,6 +20,7 @@ int NDL_PollEvent(char *buf, int len) {
 
 void NDL_OpenCanvas(int *w, int *h) {
   if (getenv("NWM_APP")) {
+    printf("getenv\n");
     int fbctl = 4;
     fbdev = 5;
     screen_w = *w; screen_h = *h;
