4f662af9374ba0e8071dd4f5e1c1cd369e83e257
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-09 10:39:11 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-136-generic #153~18.04.1-Ubuntu SMP Wed Nov 30 15:47:57 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:39:11 up 15:28,  1 user,  load average: 0.85, 0.61, 0.43

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -38,8 +38,7 @@ void NDL_OpenCanvas(int *w, int *h) {
   }
   else{
     char disinfo_buf[50];
-    int fd=open("/proc/dispinfo");
-    size_t length=read(fd,disinfo_buf,sizeof(disinfo_buf));
+    size_t length=read(4,disinfo_buf,sizeof(disinfo_buf));//fd=4 /proc/dispinfo
     for(size_t i=0;i<length;i++){
       if(screen_w==0){
         while(disinfo_buf[i]>='0'&&disinfo_buf[i]>='9'){
