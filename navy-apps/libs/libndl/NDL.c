f83a489bf0d4f386ce7ab4e508e56fade46f3504
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-10 21:59:46 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-136-generic #153~18.04.1-Ubuntu SMP Wed Nov 30 15:47:57 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:59:46 up 20 min,  1 user,  load average: 1.20, 1.07, 0.84

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -57,10 +57,11 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int Rect_startx=Canvas_startx+x;
   int Rect_starty=Canvas_starty+y;
   //3.按行顺序将像素发送到显存
-  printf("fd:%d\n",fd);
+  int fd =open("/dev/fb",0,0);
   for(int i=0;i<h;i++){
     int offset=(Rect_starty+i)*screen_w+Rect_startx;
     lseek(fd,offset,SEEK_SET);
+    printf("%d\n",i);
     write(fd,(void*)(pixels+i*w),w*sizeof(uint32_t));
   }
 }
