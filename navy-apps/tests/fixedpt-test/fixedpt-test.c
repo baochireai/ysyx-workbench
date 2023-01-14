0f0a79252126482e367d86ea1007bc865f52cce4
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-14 11:43:44 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-136-generic #153~18.04.1-Ubuntu SMP Wed Nov 30 15:47:57 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:43:44 up  2:59,  1 user,  load average: 0.71, 0.79, 0.81

--- navy-apps/tests/fixedpt-test/fixedpt-test.c
+++ navy-apps/tests/fixedpt-test/fixedpt-test.c
@@ -2,7 +2,7 @@
 #include "fixedptc.h"
 #include "math.h"
 int main() {
-  printf("floor(1.2):%d\n",(int)fixedpt_toint(fixedpt_floor(fixedpt_rconst(1))));
+  printf("floor(1.2):%d\n",(int)fixedpt_toint(fixedpt_ceil(fixedpt_ceil(1))));
   return 0;
 }
 
