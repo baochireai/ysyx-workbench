650d7eb6e1f4e1c05971880c0d35c7f7eccdb187
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-20 11:58:59 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-136-generic #153~18.04.1-Ubuntu SMP Wed Nov 30 15:47:57 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:58:59 up  1:22,  1 user,  load average: 0.71, 0.38, 0.39

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,6 +1,7 @@
 #include <NDL.h>
 #include <SDL.h>
-
+#include <stdlib.h>
+#include <string.h>
 #define keyname(k) #k,
 
 static const char *keyname[] = {
@@ -16,7 +17,22 @@ int SDL_PollEvent(SDL_Event *ev) {
   return 0;
 }
 
+
+uint8_t SDL_GetKeyFromName(const char* getkeyname){
+  for(uint8_t i=0;i<255;i++){
+    if(strcmp(getkeyname,keyname[i])==0) return i;
+  }
+  return 0;
+}
+
 int SDL_WaitEvent(SDL_Event *event) {
+  char* buf=(char*)malloc(64*sizeof(char));
+  while(!NDL_PollEvent(buf,sizeof(buf)));//kd keyname/ku keyname
+
+  event->type=(buf[0]=='k'&&buf[1]=='d')?SDL_KEYDOWN:SDL_KEYUP;
+  event->key.type=event->type;
+  printf("key:%s\n",buf+3);
+  event->key.keysym.sym=SDL_GetKeyFromName(buf+3);//keyname->keycode
   return 1;
 }
 
