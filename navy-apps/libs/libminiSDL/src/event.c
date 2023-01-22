6bcaaa6bea0f884594346c956efa443734f6e9d6
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-22 18:54:28 +0800

    > compile NEMU ysyx_22041329 薛贵 Linux mrxue 5.4.0-137-generic #154~18.04.1-Ubuntu SMP Tue Jan 10 16:58:20 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:54:28 up 1 day,  5:35,  1 user,  load average: 0.55, 0.60, 0.62

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -13,17 +13,29 @@ int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
-}
-
-
 uint8_t SDL_GetKeyFromName(const char* getkeyname){
   for(uint8_t i=0;i<83;i++){
     //printf("%d:%s\n",i,keyname[i]);
     if(strcmp(getkeyname,keyname[i])==0) return i;
   }
   return 0;
+}
+
+int SDL_PollEvent(SDL_Event *ev) {
+  char* buf=(char*)malloc(64*sizeof(char));
+  if(NDL_PollEvent(buf,64)){//kd keyname/ku keyname
+    ev->type=(buf[0]=='k'&&buf[1]=='d')?SDL_KEYDOWN:SDL_KEYUP;
+    ev->key.type=ev->type;
+    buf[strlen(buf)-1]='\0';//把\n去掉
+    printf("%s\n",buf);
+    ev->key.keysym.sym=SDL_GetKeyFromName(buf+3);//keyname->keycode
+    free(buf);
+    return 1;
+  }
+  else{
+    free(buf);
+    return 0;
+  }
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
@@ -34,6 +46,7 @@ int SDL_WaitEvent(SDL_Event *event) {
   buf[strlen(buf)-1]='\0';//把\n去掉
   printf("%s\n",buf);
   event->key.keysym.sym=SDL_GetKeyFromName(buf+3);//keyname->keycode
+  free(buf);
   return 1;
 }
 
