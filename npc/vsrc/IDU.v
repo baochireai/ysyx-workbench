6c62b660bec39559a1f8141306dd45a46f32c4f2
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-18 11:56:28 +0800

    > sim RTL ysyx_22041329 薛贵 Linux mrxue 5.19.0-45-generic #46~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 7 15:06:04 UTC 20 x86_64 x86_64 x86_64 GNU/Linux  11:56:28 up 18 min,  2 users,  load average: 0.76, 0.48, 0.49

--- npc/vsrc/IDU.v
+++ npc/vsrc/IDU.v
@@ -49,7 +49,7 @@ module IDU(
 );
 
 //(reg有数据但是将被读取|没有数据)&(当前数据处理完毕)
-assign idu_ready=((idu_valid&exu_ready)|(!idu_valid)) & (!witf_full&!isRAW);
+assign idu_ready=((idu_valid&exu_ready)|(!idu_valid)) & (!witf_full&(!isRAW|flush_pipeline));
 //exu ready busy
 //task finish busy(一周期内完成)
 //ifu valid !valid
